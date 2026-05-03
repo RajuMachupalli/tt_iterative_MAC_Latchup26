25782f62ad4c821b5520f696aa012b0aa46b39ed
Raju Machupalli <52839597+RajuMachupalli@users.noreply.github.com>
2026-05-03 13:00:21 -0400

    Rename module instance to DUT
    Updated module instantiation to use DUT naming convention.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_rajum_vga (
+  tt_um_rajum_vga DUT (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
